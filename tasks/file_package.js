7b3b7fc9ce9048c827c5d0250e9972fc44fa0c0b
robin ma <anhmzj@gmail.com>
2014-06-04 13:05:21 +0800

    mergin

--- tasks/file_package.js
+++ tasks/file_package.js
@@ -67,46 +67,6 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('file_package', 'files automatic package tools', function(arg1,arg2) {
 
-<<<<<<< HEAD
-
-    grunt.log.writeln(this.target,this.data,'===');
-
-    var srcPath=this.src;
-    grunt.
-
-
-    // Merge task-specific and/or target-specific options with these defaults.
-    // var options = this.options({
-    //   punctuation: '.',
-    //   separator: ', '
-    // });
-
-    // // Iterate over all specified file groups.
-    // this.files.forEach(function(f) {
-    //   // Concat specified files.
-    //   var src = f.src.filter(function(filepath) {
-    //     // Warn on and remove invalid source files (if nonull was set).
-    //     if (!grunt.file.exists(filepath)) {
-    //       grunt.log.warn('Source file "' + filepath + '" not found.');
-    //       return false;
-    //     } else {
-    //       return true;
-    //     }
-    //   }).map(function(filepath) {
-    //     // Read file source.
-    //     return grunt.file.read(filepath);
-    //   }).join(grunt.util.normalizelf(options.separator));
-
-    //   // Handle options.
-    //   src += options.punctuation;
-
-    //   // Write the destination file.
-    //   grunt.file.write(f.dest, src);
-
-    //   // Print a success message.
-    //   grunt.log.writeln('File "' + f.dest + '" created.');
-    // });
-=======
     //获取文件夹下所有文件
     var srcpath=this.data.src;
     if(srcpath  === '' && !srcpath){
@@ -144,7 +104,6 @@ module.exports = function(grunt) {
 
         });        
     });
->>>>>>> a20c9b4e9495754fc00fcdd486eddacf464fd2eb
 
   });
 
